3192e539a65b8b2dd431a424e67c9f053fbb6c16
longnguyencoder <long0398481719@gmail.com>
2025-02-12 00:02:20 +0700

    update form creat user

--- src/main/java/com/example/laptopshop/controller/UserController.java
+++ src/main/java/com/example/laptopshop/controller/UserController.java
@@ -3,46 +3,39 @@ package com.example.laptopshop.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.laptopshop.domain.User;
 import com.example.laptopshop.service.UserService;
-
-
-
 
 @Controller
 public class UserController {
-        private UserService userService;
+    private UserService userService;
 
-    
     public UserController(UserService userService) {
         this.userService = userService;
     }
 
     @RequestMapping("/test")
     public String getHomePage(Model model) {
-         String test = userService.handleHello();
-         model.addAttribute("eric", test);
+        String test = userService.handleHello();
+        model.addAttribute("eric", test);
+        return "hello";
+    }
+
+    @RequestMapping("/admin/user")
+    public String getUserPage(Model model) {
+        model.addAttribute("newUser", new User());
+        return "admin/user/create";
+    }
+
+    @RequestMapping(value = "/admin/user/create", method = RequestMethod.POST)
+    public String createUserPage(Model model, @ModelAttribute("newUser") User longhoccode) {
+        System.out.println("run here" + longhoccode);
         return "hello";
     }
 }
-
-// // @RestController
-// // public class UserController {
-// //     private UserService userService;
-
-    
-// //     public UserController(UserService userService) {
-// //         this.userService = userService;
-// //     }
-
-
-// //     @GetMapping("/")
-// //     public String getHomePage() {
-
-// //         return userService.handleHello();
-// //     }
-// }
