b3a5c322e3ee7c2c8939f98398c2e7a821885db2
longnguyencoder <long0398481719@gmail.com>
2025-02-11 10:17:34 +0700

    update code

---
+++ src/main/java/com/example/laptopshop/HelloController.java
@@ -0,0 +1,12 @@
+package com.example.laptopshop;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    @GetMapping("/")
+public String index() {
+return "Long bắt đầu học code";
+}
+}
