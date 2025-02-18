5845793ec4ef31b6ed8b8d06575a8d1821284bea
longnguyencoder <long0398481719@gmail.com>
2025-02-18 14:56:59 +0700

    xây dựng giao diện client template

---
+++ src/main/java/com/example/laptopshop/controller/client/HomePageController.java
@@ -0,0 +1,14 @@
+package com.example.laptopshop.controller.client;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class HomePageController {
+    @GetMapping("/")
+    public String getHomePage() {
+        return "client/homePage/show";
+    }
+
+}
